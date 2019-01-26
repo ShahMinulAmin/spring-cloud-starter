13e43bd25a882346a1fa212906b87e1805af37ff
ShahMinulAmin <sajib.iosdev@gmail.com>
2019-01-26 23:40:28 +0600

    Minor update of jungle service

--- jungle-service/src/main/java/com/sajib/example/jungleservice/web/model/AnimalDTO.java
+++ jungle-service/src/main/java/com/sajib/example/jungleservice/web/model/AnimalDTO.java
@@ -1,11 +1,13 @@
 package com.sajib.example.jungleservice.web.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * Created by sajib on 1/26/19.
  */
 @Data
+@AllArgsConstructor
 public class AnimalDTO {
     private String name;
     private String description;
